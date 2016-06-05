9d42db11cd3e4e72aac78c4b04195f98caea4f88
githubrick <libo8240@gmail.com>
2016-06-05 22:45:23 +1000

    Add files via upload

---
+++ ADDNTests/ADDNTests.swift
@@ -0,0 +1,36 @@
+//
+//  ADDNTests.swift
+//  ADDNTests
+//
+//  Created by Jack on 22/03/2015.
+//  Copyright (c) 2015 JackTraining. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ADDNTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
